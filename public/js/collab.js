af8abcb17d7e5214714352c306cffbc75ce75aae
Ionică Bizău <bizauionica@yahoo.com>
2015-02-10 18:50:35 +0200

    Create the link from the client

--- public/js/collab.js
+++ public/js/collab.js
@@ -1,3 +1,16 @@
-exports.init = function () {
-    debugger
+var $ = function (s) {
+    return document.querySelector(s);
 };
+
+
+exports.init = function () {
+    var content = $("#content");
+    this
+      .link("clientConnected")
+      .send()
+      .data(function (err, data) {
+         content.value += data + "\n";
+       })
+      ;
+};
+

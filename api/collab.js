67f9a37c3ceb615749b00d1d50fddc0a43e667e0
Ionică Bizău <bizauionica@yahoo.com>
2015-02-11 14:47:04 +0200

    Call the wsEmit method.

--- api/collab.js
+++ api/collab.js
@@ -2,7 +2,10 @@ exports.init = function () {
 };
 
 exports.clientConnectedServer = function (link) {
+        debugger
+    var foo = 1;
     setInterval(function () {
+        debugger
         link.send(null, new Date());
     }, 500);
 };
